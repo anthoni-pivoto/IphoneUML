f046e1141507a5711cf75fb207f756fc33f15cf2
anthoni-pivoto <anthonipivoto@gmail.com>
2023-10-16 23:59:43 -0300

    Finalizando Desafio
    Signed-off-by: anthoni-pivoto <anthonipivoto@gmail.com>

--- src/iphone/Iphone.java
+++ src/iphone/Iphone.java
@@ -1,4 +1,53 @@
 package iphone;
 
-public class Iphone {
+import iphone.navegador.NavegadorDeInternet;
+import iphone.reprodutor.ReprodutoMusical;
+import iphone.telefone.AparelhoTelefone;
+
+public class Iphone implements AparelhoTelefone, ReprodutoMusical, NavegadorDeInternet {
+    @Override
+    public void ligar() {
+        System.out.println("Ligando");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo");
+
+    }
+
+    @Override
+    public void iniciarCorreioDeVoz() {
+        System.out.println("Iniciando Correio De Voz");
+
+    }
+    @Override
+    public void tocar() {
+        System.out.println("Tocando");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecioando musica");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo pagina");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando pagina");
+    }
 }
